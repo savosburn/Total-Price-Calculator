bf672c39bfef9d942e63d77dc210ba1045b976a5
savosburn <savosburn18@tampabay.rr.com>
2021-06-04 14:14:02 -0400

    Initial commit

---
+++ src/main/java/ex10/App.java
@@ -0,0 +1,45 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Savannah Osburn
+ */
+
+package ex10;
+
+import java.util.Scanner;
+
+/*
+Working with multiple inputs and currency can introduce some tricky precision
+ issues.
+
+Create a simple self-checkout system. Prompt for the prices and quantities of
+ three items. Calculate the subtotal of the items.
+  Then calculate the tax using a tax rate of 5.5%.
+   Print out the line items with the quantity and total, a
+   nd then print out the subtotal, tax amount, and total.
+
+Example Output
+Enter the price of item 1: 25
+Enter the quantity of item 1: 2
+Enter the price of item 2: 10
+Enter the quantity of item 2: 1
+Enter the price of item 3: 4
+Enter the quantity of item 3: 1
+Subtotal: $64.00
+Tax: $3.52
+Total: $67.52
+
+Constraints
+Keep the input, processing, and output parts of your program separate.
+Collect the input, then do the math operations and string building, and
+then print out the output.
+Be sure you explicitly convert input to numerical data before doing any
+calculations.
+ */
+
+public class App {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
